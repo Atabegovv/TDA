5ef8ca7d750672a91350e089db676a1af2f3537a
Atabegovv <atabegov98@mail.ru>
2022-07-16 21:45:54 +0300

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,62 @@
 $(function () {
 
 
+  //header-slider
+  $('.header__slider-inner').slick({
+    infinite: true,
+    slidesToShow: 9,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1850,
+        settings: {
+          slidesToShow: 7,
+        }
+      },
+      {
+        breakpoint: 1444,
+        settings: {
+          slidesToShow: 6,
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+        }
+      },
+      {
+        breakpoint: 980,
+        settings: {
+          slidesToShow: 4,
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 400,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
+  });
+
+
   //popular-slider
   $('.popular-slider__inner').slick({
     infinite: true,
@@ -10,6 +66,26 @@ $(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1250,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 940,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 620,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
   
   //sale-slider
@@ -19,8 +95,6 @@ $(function () {
     slidesToScroll: 1,
     arrows: true,
     dots: true,
-    autoplay: true,
-    autoplaySpeed: 2000,
   });
 
    //news-slider
@@ -32,6 +106,20 @@ $(function () {
     dots: true,
     autoplay: true,
     autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+        }
+      },
+    ]
   });
 
 
